cdcef5f205ee02093120bac46828e480d68d4a1c
Brandon Rhodes <brandon@rhodesmill.org>
2012-12-03 00:03:24 -0500

    Allow system-site-packages, and add star catalog

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 cd $(dirname "${BASH_SOURCE[0]}")
-virtualenv venv
+virtualenv --system-site-packages venv
 source venv/bin/activate
 
 pip install ipython
@@ -10,3 +10,13 @@ pip install tornado
 pip install pyzmq
 
 pip install pyephem
+
+mkdir -p data
+cd data
+
+#
+# As described at http://www.astronexus.com/node/34 this is a star
+# catalog now hosted on github.  HOW COOL!
+#
+wget -O hygfull.csv -c \
+  https://github.com/astronexus/HYG-Database/blob/master/hygfull.csv?raw=true
