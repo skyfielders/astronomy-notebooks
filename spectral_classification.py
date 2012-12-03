b1eeb8d546d568ac06d4800940fddc349bd9c94a
Brandon Rhodes <brandon@rhodesmill.org>
2012-12-03 11:51:10 -0500

    Be more lax about spectral classification matches

--- spectral_classification.py
+++ spectral_classification.py
@@ -1,5 +1,13 @@
 """Convert stellar spectral classes to RGB values."""
 
+from numpy import array
+
+def without_v(s):
+    return s.replace('(V)', '')
+
+def without_parens(s):
+    return s.replace('(', '').replace(')', '')
+
 def build_color_chart(path):
     chart = {}
 
@@ -9,7 +17,9 @@ def build_color_chart(path):
                 continue
             fields = line.split()
             spectral_class = fields[0]
-            rgb = tuple(float(field) / 255.0 for field in fields[3:6])
+            rgb = array([ float(field) / 255.0 for field in fields[3:6] ])
             chart[spectral_class] = rgb
+            chart[without_v(spectral_class)] = rgb
+            chart[without_parens(spectral_class)] = rgb
 
     return chart
