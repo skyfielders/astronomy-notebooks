6839d1f524596635c25965e06197c7ae565065dd
Brandon Rhodes <brandon@rhodesmill.org>
2013-01-01 18:42:58 -0700

    Add setup commands to fetch JPL asteroids, comets

--- setup.sh
+++ setup.sh
@@ -14,9 +14,14 @@ pip install pyephem
 mkdir -p data
 cd data
 
-#
-# As described at http://www.astronexus.com/node/34 this is a star
-# catalog now hosted on github.  HOW COOL!
-#
+# As described on the http://www.astronexus.com/node/34 page, the
+# following link is a star catalog now hosted on github.  HOW COOL!
+
 wget -O hygfull.csv -c \
   https://github.com/astronexus/HYG-Database/blob/master/hygfull.csv?raw=true
+
+# Asteroids and comets.
+
+wget -c http://ssd.jpl.nasa.gov/dat/ELEMENTS.NUMBR.gz
+wget -c http://ssd.jpl.nasa.gov/dat/ELEMENTS.UNNUM.gz
+wget -c http://ssd.jpl.nasa.gov/dat/ELEMENTS.COMET
