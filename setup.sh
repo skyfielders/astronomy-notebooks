a12669bd1c848fa1c071a1f835086cd5a97f2c4e
Brandon Rhodes <brandon@rhodesmill.org>
2013-01-08 20:21:33 -0500

    Add de405 to setup.sh and make setup.sh rerunnable

--- setup.sh
+++ setup.sh
@@ -6,21 +6,28 @@ cd $(dirname "${BASH_SOURCE[0]}")
 if [ ! -d venv ]
 then
     virtualenv --system-site-packages venv
-    source venv/bin/activate
-
-    pip install basemap
-    pip install ipython
-    pip install mayavi
-    # pip install pandas
-    # pip install tables
-    pip install tornado
-    pip install pyzmq
-
-    pip install pyephem
-
-    mkdir -p data
 fi
 
+source venv/bin/activate
+
+#pip install basemap
+pip install ipython
+pip install mayavi
+# pip install pandas
+# pip install tables
+pip install tornado
+pip install pyzmq
+
+pip install pyephem
+
+# And since I am still developing and tweaking them at the moment:
+pip install -e ~/jplephem/jplephem
+pip install -e ~/jplephem/de405
+pip install -e ~/sgp4
+
+# Get ready to download large data sets.
+
+mkdir -p data
 cd data
 
 # As described on the http://www.astronexus.com/node/34 page, the
