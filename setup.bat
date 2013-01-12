65347d5b5ad0febc33e2a6e8d6a8e3f8b3a550df
Barbara Jones <barbarajoyjones@gmail.com>
2013-01-12 17:31:52 -0500

    start a setup script for windows

---
+++ setup.bat
@@ -0,0 +1,28 @@
+REM TODO set up a new virtualenv and activate it
+
+REM iPython, and the libraries needed for it to run Notebook.
+
+pip install ipython
+pip install tornado
+pip install pyzmq
+
+REM Visualization tools and their dependencies.
+
+pip install numpy
+pip install scipy
+pip install matplotlib
+pip install vtk
+pip install mayavi
+pip install wxPython
+
+REM Tools specifically for the 'iPython Features' notebook.
+
+pip install sympy
+
+REM Astronomical software.
+
+pip install pyephem
+
+REM TODO install jplephem, de405, sgp4
+
+REM TODO download data sets
