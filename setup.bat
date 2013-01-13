846d51f61575195a950f14c24885811b712640d2
Barbara Jones <barbarajoyjones@gmail.com>
2013-01-12 23:15:53 -0500

    Minor spacing

--- setup.bat
+++ setup.bat
@@ -3,13 +3,11 @@ if not exist venv virtualenv --system-site-packages venv
 venv\Scripts\activate
 
 REM iPython, and the libraries needed for it to run Notebook.
-
 pip install ipython
 pip install tornado
 pip install pyzmq
 
 REM Visualization tools and their dependencies.
-
 pip install numpy
 pip install scipy
 pip install matplotlib
@@ -18,11 +16,9 @@ pip install mayavi
 pip install wxPython
 
 REM Tools specifically for the 'iPython Features' notebook.
-
 pip install sympy
 
 REM Astronomical software.
-
 pip install pyephem
 
 REM TODO install jplephem, de405, sgp4
