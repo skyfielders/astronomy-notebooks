ac786fe32fb4708dc2878400d44652071233f31d
Brandon Rhodes <brandon@rhodesmill.org>
2015-04-15 15:59:33 -0400

    Remove out-of-date data downloads from "setup.sh"

--- setup.sh
+++ setup.sh
@@ -39,14 +39,7 @@ pip install \
 mkdir -p data
 cd data
 
-# As described on the http://www.astronexus.com/node/34 page, the
-# following link is a star catalog now hosted on github.  HOW COOL!
-wget -nc https://raw.github.com/astronexus/HYG-Database/master/hygfull.csv
-
-# Asteroids and comets.
-wget -nc http://ssd.jpl.nasa.gov/dat/ELEMENTS.NUMBR.gz
-wget -nc http://ssd.jpl.nasa.gov/dat/ELEMENTS.UNNUM.gz
-wget -nc http://ssd.jpl.nasa.gov/dat/ELEMENTS.COMET
-
-# Earth satellites.
-wget -nc http://www.celestrak.com/NORAD/elements/visual.txt
+echo
+echo 'Setup successful!'
+echo 'Run "download_data.py" to make sure your "data" directory is populated.'
+echo
