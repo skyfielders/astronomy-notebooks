e51e8b06cd2f68f44590b057e436e33bb6dc1fa6
Brandon Rhodes <brandon@rhodesmill.org>
2020-12-19 18:04:19 -0500

    Teach report to compute Solar System size

--- Medieval/report.py
+++ Medieval/report.py
@@ -5,20 +5,31 @@ import json
 import numpy as np
 import os
 import sys
-from glob import glob
+
+planets = 'Moon Mercury Venus Sun Mars Jupiter Saturn'.split()
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Print out eccentricities')
     parser.parse_args(argv)
 
-    paths = glob('parameters_*.txt')
-    for path in sorted(paths):
+    radius = 59.7  # Ptolemy’s estimate of Moon’s distance in Earth radii
+
+    for planet_name in planets:
+        path = f'parameters_{planet_name}.txt'
         with open(path) as f:
             params = json.load(f)
 
         DT, D0, xe, ye, ET, E0, Er = params
         eccentricity = np.sqrt(xe * xe + ye * ye)
-        print(f'{path:30} {eccentricity:5.3f}')
+
+        closest = 1.0 - eccentricity - Er
+        farthest = 1.0 + eccentricity + Er
+        outer_radius = radius * farthest / closest
+
+        print(f'{path:30} {eccentricity:5.3f}    '
+              f'{radius:.2f} - {outer_radius:.2f}')
+
+        radius = outer_radius
 
 if __name__ == '__main__':
     main(sys.argv[1:])
